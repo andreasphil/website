7be611e0c7e834a7212ab51ef803e2f596c2c2bc
Andreas Philippi <18646892+andrsphil@users.noreply.github.com>
2019-11-17 17:38:23 +0100

    Redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,39 @@ module.exports = {
   theme: {
     extend: {
       width: {
-        'breakout-small': 'calc(100% + 2rem)',
-        'breakout-large': 'calc(100% + 4rem)'
+        'breakout-1': 'calc(100% + 2rem)',
+        'breakout-2': 'calc(100% + 4rem)',
       },
       maxWidth: {
-        'breakout-small': 'calc(100% + 2rem)',
-        'breakout-large': 'calc(100% + 4rem)'
+        'breakout-1': 'calc(100% + 2rem)',
+        'breakout-2': 'calc(100% + 4rem)',
       }
     },
 
     spacing: {
       px: '1px',
       '0': '0',
-      '1': '.125rem',
-      '2': '.5rem',
-      '3': '1rem',
-      '4': '2rem',
-      '5': '4rem',
-      '6': '8rem'
+      '1/4': '.25rem',
+      '1/2': '.5rem',
+      '3/4': '.75rem',
+      '1': '1rem',
+      '1-1/2': '1.5rem',
+      '2': '2rem',
+      '4': '4rem',
+      '6': '6rem',
+      '8': '8rem',
+      'max': '1024px'
     },
     screens: {
-      small: '480px',
-      normal: '640px'
+      small: '640px',
+      normal: '960px'
+    },
+    container: {
+      center: true
     },
     fontFamily: {
       sans: [
+        'IBM Plex Sans',
         'Inter',
         '-apple-system',
         '"Helvetica Neue"',
@@ -35,32 +43,30 @@ module.exports = {
       ]
     },
     fontSize: {
-      tiny: '.875rem',
-      small: '1rem',
-      base: '1.125rem',
-      large: '1.25rem',
+      small: '.875rem',
+      base: '1rem',
+      large: '1.125rem',
       h1: '2rem',
       h2: '1.5rem',
-      h3: '1.25rem',
     },
     fontWeight: {
       normal: '400',
-      emphasized: '500',
-      heavy: '600'
+      emphasized: '600'
     },
     lineHeight: {
       none: '1',
-      tight: '1.5',
+      tight: '1.25',
       normal: '1.75'
     },
     borderRadius: {
       default: '.2rem',
       none: '0',
-      small: '.125rem',
       full: '9999rem'
     }
   },
+
   variants: {},
+
   plugins: [
     require('tailwindcss-transition')({
       standard: 'all .3s ease'
